6b7cd2e710dc503c948933d6903bdcc76fb3c07d
Nicolas Pautasso <nicopautasso95@gmail.com>
2021-07-28 09:25:43 -0300

    migrate db heroku fix v2

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,6 @@ ActiveRecord::Schema.define(version: 2021_07_26_135457) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
-    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
